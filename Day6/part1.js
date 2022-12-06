a12f483259d92124f8fdcc7f8f2181c5cc9f8220
Konrad Romanowski <konrad.romanowski.it@gmail.com>
2022-12-06 19:53:27 +0100

    Added Day6 solutions

---
+++ Day6/part1.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+
+const inputPath = path.join(__dirname,'input.txt');
+
+fs.readFile(inputPath,'utf-8', (err,signal) =>{
+    if(err) {
+        console.log(`Cannot open the file: ${inputPath}`)
+    } else {
+        let indexCounter = 4;
+        const currentFourSigns = {};
+
+        for(let i = 0; i < indexCounter; i++) {
+            currentFourSigns.hasOwnProperty(signal[i]) ? currentFourSigns[signal[i]] += 1 : currentFourSigns[signal[i]] = 1;
+        }
+
+        while(indexCounter < signal.length) {
+            if(Object.values(currentFourSigns).every(value => value <= 1)) {
+                console.log(indexCounter);
+                break;
+            }
+
+            currentFourSigns[signal[indexCounter-4]] -= 1
+            currentFourSigns.hasOwnProperty(signal[indexCounter]) ? currentFourSigns[signal[indexCounter]] += 1 : currentFourSigns[signal[indexCounter]] = 1;
+            
+            indexCounter++;
+        }
+    }
+});
